cfa518819e4ec844e16003610e6f07f58670263c
Gustavo Bruno <gugubrunopf@gmail.com>
2025-08-08 10:37:00 -0300

    fix: changing <a> to <Link>

--- src/components/ui/navbar.tsx
+++ src/components/ui/navbar.tsx
@@ -1,11 +1,21 @@
+import Link from "next/link";
+
 export function Navbar() {
   return (
     <nav className="navbar flex w-auto justify-center items-center bg-[var(--azul-escurecido)] p-3 rounded-2xl">
       <ul className="flex gap-8 text-xl">
-        <li className="transform hover:scale-110 transition duration-300 ease-in-out"><a href="/">Início</a></li>
-        <li className="transform hover:scale-110 transition duration-300 ease-in-out"><a href="/servicos">Serviços</a></li>
-        <li className="transform hover:scale-110 transition duration-300 ease-in-out"><a href="/sobre">Sobre Nós</a></li>
-        <li className="transform hover:scale-110 transition duration-300 ease-in-out"><a href="/contato">Contato</a></li>
+        <li className="transform hover:scale-110 transition duration-300 ease-in-out">
+          <Link href="/">Início</Link>
+        </li>
+        <li className="transform hover:scale-110 transition duration-300 ease-in-out">
+          <Link href="/servicos">Serviços</Link>
+        </li>
+        <li className="transform hover:scale-110 transition duration-300 ease-in-out">
+          <Link href="/sobre">Sobre Nós</Link>
+        </li>
+        <li className="transform hover:scale-110 transition duration-300 ease-in-out">
+          <Link href="/contato">Contato</Link>
+        </li>
       </ul>
     </nav>  
   );
